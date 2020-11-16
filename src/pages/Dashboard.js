c60d24dde42b43eccd223789f8cb9de688666365
nuzulzen <nuzulzen@gmail.com>
2020-11-16 12:45:59 +0700

    fix structure

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,15 +1,27 @@
 import { useContext, useState } from "react";
+import Collection from "../components/Collection";
+import NearbyRestaurant from "../components/NearbyRestaurant";
 import Restaurant from "../components/Restaurant";
 import SearchForm from "../components/SearchForm";
 import { RestaurantContext } from "../context/context";
 
 function Dashboard() {
   const { restaurants } = useContext(RestaurantContext);
-  console.log("data", restaurants);
+
   return (
     <div className="container">
       {restaurants.map((rests) => {
-        return <SearchForm key={rests.location.city_id} rests={rests} />;
+        console.log("rests", rests);
+        const data = rests.nearby_restaurants;
+        console.log("data", data);
+        return (
+          <>
+            {" "}
+            <SearchForm location={rests.location.city_name} />
+            <Collection />
+            <NearbyRestaurant key={rests.location.city_id} collection={data} />
+          </>
+        );
       })}
     </div>
   );
