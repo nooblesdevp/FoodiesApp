6ac01fac53c8e10bb7909fbf5d87efaa80877af5
nuzulzen <nuzulzen@gmail.com>
2020-11-16 12:46:20 +0700

    add mockupCollection.js

---
+++ src/context/mockData/mockCollection.js
@@ -0,0 +1,488 @@
+export default [
+  {
+    collections: [
+      {
+        collection: {
+          collection_id: 306459,
+          res_count: 61,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/ee162d6ec5d1d40f0358325e6b6242c6_1584525552.jpg",
+          url:
+            "https://www.zomato.com/jakarta/makan-di-rumah?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Makan di Rumah",
+          description:
+            "Temukan restoran dengan layanan pesan antar makanan di bawah ini. Tetap sehat dan kenyang, #DiRumahAja!",
+          share_url: "http://www.zoma.to/c-74/306459",
+        },
+      },
+      {
+        collection: {
+          collection_id: 306729,
+          res_count: 26,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/582eed9154d5898a4b9dd7925214c3eb_1600772424.jpg",
+          url:
+            "https://www.zomato.com/jakarta/makan-di-mobil?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Makan di Mobil",
+          description:
+            "Can't dine in? No worries, these restaurants serve you food, fresh in your car, right in the car park.",
+          share_url: "http://www.zoma.to/c-74/306729",
+        },
+      },
+      {
+        collection: {
+          collection_id: 274852,
+          res_count: 238,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/7c831cd5f20ca9d77257fb5e218f5003_1551247891.jpg",
+          url:
+            "https://www.zomato.com/jakarta/great-food-no-bull?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Jakarta's Finest",
+          description: "Deliciousness guaranteed.",
+          share_url: "http://www.zoma.to/c-74/274852",
+        },
+      },
+      {
+        collection: {
+          collection_id: 29,
+          res_count: 11,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/f057387e754a31ee15c79bc5cfa988e9_1574739735.jpg",
+          url:
+            "https://www.zomato.com/jakarta/newly-opened?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "New on Zomato!",
+          description: "Find out what's new in Jakarta here",
+          share_url: "http://www.zoma.to/c-74/29",
+        },
+      },
+      {
+        collection: {
+          collection_id: 306749,
+          res_count: 31,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/3be38c5cdbee855f0fe593904ade5313_1601883494.jpg",
+          url:
+            "https://www.zomato.com/jakarta/best-ricebowl-in-j-town?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Best Rice Bowl in J-Town",
+          description: "The best rice bowl you should try in Jakarta!",
+          share_url: "http://www.zoma.to/c-74/306749",
+        },
+      },
+      {
+        collection: {
+          collection_id: 71,
+          res_count: 89,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/ca5ff499936e57ee06a6d1061efe0512_1596532601.jpg",
+          url:
+            "https://www.zomato.com/jakarta/authentic-indonesian-restaurants?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Indonesia Represents",
+          description: "Indonesia's most delicious traditional eats.",
+          share_url: "http://www.zoma.to/c-74/71",
+        },
+      },
+      {
+        collection: {
+          collection_id: 306754,
+          res_count: 23,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/8dce3befcfbd10d456fd8ba4fc3c09a5_1602502213.jpg",
+          url:
+            "https://www.zomato.com/jakarta/suburban-gems?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Suburban Gems",
+          description:
+            "Looking for restaurant outside Jakarta? Try these restaurants!",
+          share_url: "http://www.zoma.to/c-74/306754",
+        },
+      },
+      {
+        collection: {
+          collection_id: 22,
+          res_count: 82,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/14ad9658199036d32f9be6d8d40b0025_1567591009.jpg",
+          url:
+            "https://www.zomato.com/jakarta/best-coffee?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Coffee Cravings",
+          description: "Fulfill your daily coffee intake here",
+          share_url: "http://www.zoma.to/c-74/22",
+        },
+      },
+      {
+        collection: {
+          collection_id: 537,
+          res_count: 76,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/2d4e7d2c354dc960f0775d4766b1b4b7_1567586337.jpg",
+          url:
+            "https://www.zomato.com/jakarta/all-you-can-eat?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "All You Can Eat!",
+          description: "Let's have a dare: Whoever eats the most, wins!",
+          share_url: "http://www.zoma.to/c-74/537",
+        },
+      },
+      {
+        collection: {
+          collection_id: 92,
+          res_count: 22,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/9c0b6e018ae12dfc7c370cf714b7fae2_1568086635.jpg",
+          url:
+            "https://www.zomato.com/jakarta/cityscapes?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Up in the Sky",
+          description: "Have a dine in experience like no other",
+          share_url: "http://www.zoma.to/c-74/92",
+        },
+      },
+      {
+        collection: {
+          collection_id: 71343,
+          res_count: 40,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/94ff773bf1d8dad6aed1697af6540d19_1567499827.jpg",
+          url:
+            "https://www.zomato.com/jakarta/insta-worthy?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Picture Perfect",
+          description: "Take your best shots while sipping lattes",
+          share_url: "http://www.zoma.to/c-74/71343",
+        },
+      },
+      {
+        collection: {
+          collection_id: 3,
+          res_count: 16,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/6518dd218345af35cc0262e1633b2466_1568010988.jpg",
+          url:
+            "https://www.zomato.com/jakarta/romantic-restaurants?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Romance Me",
+          description: "Indulge in your best nights here",
+          share_url: "http://www.zoma.to/c-74/3",
+        },
+      },
+      {
+        collection: {
+          collection_id: 225,
+          res_count: 14,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/dc991fcf91340dedba11433c1ff71fc6_1568100861.jpg",
+          url:
+            "https://www.zomato.com/jakarta/fine-dining-restaurants?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Luxury Dining",
+          description: "Splurge it your way.",
+          share_url: "http://www.zoma.to/c-74/225",
+        },
+      },
+      {
+        collection: {
+          collection_id: 7,
+          res_count: 81,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/21a5e7a5271b4057bfc0cdcad6860f44_1567509152.jpg",
+          url:
+            "https://www.zomato.com/jakarta/legendary-places?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Through Histories",
+          description: "The list of restaurants probably older than you!",
+          share_url: "http://www.zoma.to/c-74/7",
+        },
+      },
+      {
+        collection: {
+          collection_id: 126,
+          res_count: 42,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/c5e634df5fc33de6a647ac508fba81f9_1568088274.jpg",
+          url:
+            "https://www.zomato.com/jakarta/korean-bbq?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Best Korean BBQ",
+          description: "Annyeonghaseyo, let's eat some good meat!",
+          share_url: "http://www.zoma.to/c-74/126",
+        },
+      },
+      {
+        collection: {
+          collection_id: 71250,
+          res_count: 22,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/012e37a5591452b9cc0be4560a2f96a6_1518176130.jpg",
+          url:
+            "https://www.zomato.com/jakarta/best-of-kaki5jkt?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Straight from Streets",
+          description:
+            "Craving for street foods? Explore Jakarta's most appetizing street vendors here",
+          share_url: "http://www.zoma.to/c-74/71250",
+        },
+      },
+      {
+        collection: {
+          collection_id: 114828,
+          res_count: 19,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/98b8dec611472e4b36483f6a0162da43_1550728101.jpg",
+          url:
+            "https://www.zomato.com/jakarta/weekend-getaway-spots?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Weekend Getaway Spots in Bogor",
+          description: "Escape the hustle and bustle of the city",
+          share_url: "http://www.zoma.to/c-74/114828",
+        },
+      },
+      {
+        collection: {
+          collection_id: 55,
+          res_count: 34,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/f090ef9e00f26b6148e22bb8dd753f78_1568016300.jpg",
+          url:
+            "https://www.zomato.com/jakarta/live-music-restaurants?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Live Music",
+          description: "Best live music venues in Jakarta",
+          share_url: "http://www.zoma.to/c-74/55",
+        },
+      },
+      {
+        collection: {
+          collection_id: 39,
+          res_count: 99,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/310e355a01ac5376c512cab27b29a928_1581498461.jpg",
+          url:
+            "https://www.zomato.com/jakarta/healthy-restaurants?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "#EatClean",
+          description: "What you eat is what you'll be!",
+          share_url: "http://www.zoma.to/c-74/39",
+        },
+      },
+      {
+        collection: {
+          collection_id: 250,
+          res_count: 48,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/f8c60208d3b46cfff86494b6f57e544b_1568021535.jpg",
+          url:
+            "https://www.zomato.com/jakarta/best-bars-in-the-city?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Let's Have a Toast!",
+          description: "Unwind yourself at some of our best curated bars",
+          share_url: "http://www.zoma.to/c-74/250",
+        },
+      },
+      {
+        collection: {
+          collection_id: 35,
+          res_count: 69,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/b69971c595adb0e1f2413e648d56ecb2_1568017523.jpg",
+          url:
+            "https://www.zomato.com/jakarta/ramen-restaurants?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Ramen Rally",
+          description: "Never say no for ramen.",
+          share_url: "http://www.zoma.to/c-74/35",
+        },
+      },
+      {
+        collection: {
+          collection_id: 5,
+          res_count: 32,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/a8d030cce574ca11d29122d506268e3a_1552449292.jpg",
+          url:
+            "https://www.zomato.com/jakarta/best-burger?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Kickass Burgers",
+          description: "Chow down on a cheeselicious burger!",
+          share_url: "http://www.zoma.to/c-74/5",
+        },
+      },
+      {
+        collection: {
+          collection_id: 47,
+          res_count: 35,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/2a48c3fb3fb5da71f48683ac7646d8f6_1568101655.jpg",
+          url:
+            "https://www.zomato.com/jakarta/steakhouses?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Steak Struck",
+          description: "Medium-rare or well-done? Steak your pick!",
+          share_url: "http://www.zoma.to/c-74/47",
+        },
+      },
+      {
+        collection: {
+          collection_id: 675,
+          res_count: 77,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/e1a2a2e7eeb952ded097ecf5e211a727_1581501524.jpg",
+          url:
+            "https://www.zomato.com/jakarta/best-bakmie?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Bakmi, Babies!",
+          description: "Noodles and chicken - match-made in heaven!",
+          share_url: "http://www.zoma.to/c-74/675",
+        },
+      },
+      {
+        collection: {
+          collection_id: 321,
+          res_count: 48,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/6b23a95cfb9c6849ae2461ed12a54834_1552301610.jpg",
+          url:
+            "https://www.zomato.com/jakarta/pizza-time?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Pizza Perfect",
+          description: "The best pizza places in town",
+          share_url: "http://www.zoma.to/c-74/321",
+        },
+      },
+      {
+        collection: {
+          collection_id: 52,
+          res_count: 46,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/cca7a5e3bd5ce1a852cb391ad61b0ac4_1551781472.jpg",
+          url:
+            "https://www.zomato.com/jakarta/frozen-delights?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Amazingly Sweet!",
+          description: "Some sweetness never hurts anybody!",
+          share_url: "http://www.zoma.to/c-74/52",
+        },
+      },
+      {
+        collection: {
+          collection_id: 248,
+          res_count: 29,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/b7c7c1cda6f8ef75f3a091c39edeaa9c_1552449526.jpg",
+          url:
+            "https://www.zomato.com/jakarta/best-dimsums?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Dim Sum Dreams",
+          description: "Little pockets of delicious steamed goodness",
+          share_url: "http://www.zoma.to/c-74/248",
+        },
+      },
+      {
+        collection: {
+          collection_id: 536,
+          res_count: 38,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/e304a6243421cff3d7311594554448bc_1581497307.jpg",
+          url:
+            "https://www.zomato.com/jakarta/function-venues?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Let's Get Festive!",
+          description: "Restaurants for large parties or family functions!",
+          share_url: "http://www.zoma.to/c-74/536",
+        },
+      },
+      {
+        collection: {
+          collection_id: 30,
+          res_count: 50,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/830ab3b16c4cb210ef8e47954d9413c4_1551247350.jpg",
+          url:
+            "https://www.zomato.com/jakarta/child-friendly-restaurants?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Child-Friendly Restaurants",
+          description: "Perfect places for dining with the entire family",
+          share_url: "http://www.zoma.to/c-74/30",
+        },
+      },
+      {
+        collection: {
+          collection_id: 25,
+          res_count: 18,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/b808f41eb41d8154eb16d084382f380c_1548671603.jpg",
+          url:
+            "https://www.zomato.com/jakarta/pet-friendly-restaurants?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Pet-Friendly",
+          description:
+            "A pet lover? Chill out with your furry best friend at these spots ",
+          share_url: "http://www.zoma.to/c-74/25",
+        },
+      },
+      {
+        collection: {
+          collection_id: 20,
+          res_count: 51,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/532af3ab24d29dbddcdcd16921ada06f_1535628640.jpg",
+          url:
+            "https://www.zomato.com/jakarta/great-breakfasts?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Great Breakfasts",
+          description:
+            "Brekkie or brunch, regardless what tickles your fancy, immerse yourself in our city’s cafe culture at these top spots",
+          share_url: "http://www.zoma.to/c-74/20",
+        },
+      },
+      {
+        collection: {
+          collection_id: 65,
+          res_count: 59,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/73391cc46aa0d33d5c5cf3bd09981bc6_1469073198.jpg",
+          url:
+            "https://www.zomato.com/jakarta/sushi?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Rolling in the Sushi",
+          description: "Get a load of these scrumptious sushi delights",
+          share_url: "http://www.zoma.to/c-74/65",
+        },
+      },
+      {
+        collection: {
+          collection_id: 2,
+          res_count: 19,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/9301edee2f248170d58fce6693291cfd_1551165546.jpg",
+          url:
+            "https://www.zomato.com/jakarta/sunday-brunch?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Sunday Brunches",
+          description: "Just about the perfect way to end the week",
+          share_url: "http://www.zoma.to/c-74/2",
+        },
+      },
+      {
+        collection: {
+          collection_id: 310,
+          res_count: 16,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/08810aa7d86f8e08743cfadf063b3bb5_1418715961_l.jpg",
+          url:
+            "https://www.zomato.com/jakarta/board-game-cafe?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Come Dine and Play!",
+          description:
+            "Board game enthusiasts - Let's play together and have some fun!",
+          share_url: "http://www.zoma.to/c-74/310",
+        },
+      },
+      {
+        collection: {
+          collection_id: 249,
+          res_count: 12,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/b961d73b7f0cd325174b23d23ea0d215_1418400165_l.jpg",
+          url:
+            "https://www.zomato.com/jakarta/magnificent-martabak?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Martabak Marvel",
+          description:
+            "Sweet or salty? Your call! Check these places out for a nice & juicy martabak experience",
+          share_url: "http://www.zoma.to/c-74/249",
+        },
+      },
+      {
+        collection: {
+          collection_id: 303026,
+          res_count: 15,
+          image_url:
+            "https://b.zmtcdn.com/data/collections/6539d467b1d38d03b6ba74f07a543171_1570692139.jpg",
+          url:
+            "https://www.zomato.com/jakarta/feast-from-the-east?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+          title: "Feast from the East",
+          description:
+            "Selected East Indonesian dish that you would not want to miss!",
+          share_url: "http://www.zoma.to/c-74/303026",
+        },
+      },
+    ],
+    has_more: 0,
+    share_url: "http://www.zoma.to/c-74",
+    display_text: "OR EXPLORE OUR COLLECTIONS",
+    has_total: 0,
+    user_has_addresses: true,
+  },
+];
