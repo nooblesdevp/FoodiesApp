07522f541c08f5297d445dd3c87c7312a38830db
nuzulzen <nuzulzen@gmail.com>
2020-11-19 16:10:21 +0700

    add mockupRestaurant contex

--- src/context/context.js
+++ src/context/context.js
@@ -1,15 +1,22 @@
 import React, { createContext, useState } from "react";
 import mockCollection from "./mockData/mockCollection";
 import mockLocation from "./mockData/mockLocation";
+import mockRestaurantProfile from "./mockData/mockRestaurantProfile";
 
 const RestaurantContext = createContext();
+
 function RestaurantProvider({ children }) {
   const [restaurants, setRestaurants] = useState(mockLocation);
   const [collection, setCollection] = useState(mockCollection);
   const [isLoading, setIsLoading] = useState(true);
+  const [restauranProfile, setRestaurantProfile] = useState(
+    mockRestaurantProfile
+  );
 
   return (
-    <RestaurantContext.Provider value={{ restaurants, collection, isLoading }}>
+    <RestaurantContext.Provider
+      value={{ restaurants, collection, isLoading, restauranProfile }}
+    >
       {children}
     </RestaurantContext.Provider>
   );
