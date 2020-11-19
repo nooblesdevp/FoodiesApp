a150fe428f6dd43a7be061462a2a66e438759f1e
nuzulzen <nuzulzen@gmail.com>
2020-11-19 16:10:54 +0700

    add mockup Restaurant data

---
+++ src/context/mockData/mockRestaurantProfile.js
@@ -0,0 +1,105 @@
+export default {
+  R: {
+    res_id: 18366214,
+    is_grocery_store: false,
+    has_menu_status: {
+      delivery: -1,
+      takeaway: -1,
+    },
+  },
+  apikey: "cf104aad3040f82886c92e6d71e36b05",
+  id: "18366214",
+  name: "Wing Heng Hongkong Dim Sum Shop",
+  url:
+    "https://www.zomato.com/jakarta/wing-heng-hongkong-dim-sum-shop-pluit?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+  location: {
+    address: "Jl. Pluit Karang Utara, Blok A4U No. 65A, Pluit, Jakarta",
+    locality: "Pluit",
+    city: "Jakarta",
+    city_id: 74,
+    latitude: "-6.1122940779",
+    longitude: "106.7808950320",
+    zipcode: "",
+    country_id: 94,
+    locality_verbose: "Pluit, Jakarta",
+  },
+  switch_to_order_menu: 0,
+  cuisines: "Dimsum, Chinese",
+  timings: "24 Hours",
+  average_cost_for_two: 150000,
+  price_range: 3,
+  currency: "IDR",
+  highlights: [
+    "Takeaway Available",
+    "No Alcohol Available",
+    "Dinner",
+    "Cash",
+    "Breakfast",
+    "Lunch",
+    "Debit Card",
+    "Indoor Seating",
+    "Air Conditioned",
+  ],
+  offers: [],
+  opentable_support: 0,
+  is_zomato_book_res: 0,
+  mezzo_provider: "OTHER",
+  is_book_form_web_view: 0,
+  book_form_web_view_url: "",
+  book_again_url: "",
+  thumb:
+    "https://b.zmtcdn.com/data/reviews_photos/19e/c13447c5e01f69021b4d53829c5b619e_1530292565.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A",
+  user_rating: {
+    aggregate_rating: "4.5",
+    rating_text: "Excellent",
+    rating_color: "3F7E00",
+    rating_obj: {
+      title: {
+        text: "4.5",
+      },
+      bg_color: {
+        type: "lime",
+        tint: "700",
+      },
+    },
+    votes: 1247,
+  },
+  all_reviews_count: 699,
+  photos_url:
+    "https://www.zomato.com/jakarta/wing-heng-hongkong-dim-sum-shop-pluit/photos?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1#tabtop",
+  photo_count: 1722,
+  menu_url:
+    "https://www.zomato.com/jakarta/wing-heng-hongkong-dim-sum-shop-pluit/menu?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1&openSwipeBox=menu&showMinimal=1#tabtop",
+  featured_image:
+    "https://b.zmtcdn.com/data/reviews_photos/19e/c13447c5e01f69021b4d53829c5b619e_1530292565.jpg",
+  has_online_delivery: 0,
+  is_delivering_now: 0,
+  store_type: "",
+  include_bogo_offers: true,
+  deeplink: "zomato://restaurant/18366214",
+  is_table_reservation_supported: 0,
+  has_table_booking: 0,
+  events_url:
+    "https://www.zomato.com/jakarta/wing-heng-hongkong-dim-sum-shop-pluit/events#tabtop?utm_source=api_basic_user&utm_medium=api&utm_campaign=v2.1",
+  phone_numbers: "0838 75116699",
+  all_reviews: {
+    reviews: [
+      {
+        review: [],
+      },
+      {
+        review: [],
+      },
+      {
+        review: [],
+      },
+      {
+        review: [],
+      },
+      {
+        review: [],
+      },
+    ],
+  },
+  establishment: ["Quick Bites"],
+};
