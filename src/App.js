bda0be9544de38073bff0fe4dd636b1ccaa936dd
nuzulzen <nuzulzen@gmail.com>
2020-11-20 17:23:36 +0700

    fix path with exact

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           <Route path="/restaurant/profile/:id">
             <RestaurantProfile />
           </Route>
-          <Route path="/">
+          <Route path="/" exact>
             <Dashboard />
           </Route>
         </div>
